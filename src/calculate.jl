fe0ec501ee09ad8b373863932feb96de751b5086
bocklund <brandonbocklund@gmail.com>
2019-09-04 08:33:49 -0400

    Don't pre-fill the phase values array

--- src/calculate.jl
+++ src/calculate.jl
@@ -7,7 +7,7 @@ function compute_phase_values(statevar_dict, prx, points)
     statevars = sort!(collect(keys(svd)))
     statevars_shape = Tuple([length(svd[sv]) for sv in statevars])
     nbpts = size(points)[1]
-    vals = fill(NaN, statevars_shape..., nbpts)
+    vals = Array{Float64}(undef, statevars_shape..., nbpts)
     for idx in CartesianIndices(statevars_shape)
         for pt in 1:nbpts
             vals[idx, pt] = prx.obj([svd[statevars[i]][idx[i]] for i in 1:length(idx)]..., points[pt, :]...)
