bae167f0e60fb3616e4593ad932f6c4100e0e8ce
bocklund <brandonbocklund@gmail.com>
2019-09-05 08:12:21 -0400

    Code cleanup

--- src/result.jl
+++ src/result.jl
@@ -2,9 +2,9 @@ abstract type Result end
 
 struct Grid <: Result
     X::Array{Float64, 2}
-    Y::Array{Float64,2 }
+    Y::Array{Float64, 2}
     Phase::Array{String, 1}
     output::Array{Float64, 2}
     points::Array{Int64, 1}  # indices
-    statevars::Dict{String, Array{Float64,1 }}
+    statevars::Dict{String, Array{Float64, 1}}
 end
