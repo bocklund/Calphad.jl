c05d31c50238d3d9ce36f9dca3021058973eb8bd
bocklund <brandonbocklund@gmail.com>
2021-03-20 11:06:17 -0400

    Comment out test code that isn't validated yet

--- test/symbolic-solver.jl
+++ test/symbolic-solver.jl
@@ -52,73 +52,73 @@ println("Expected chemical potentials: ", [1542.0966603, 8282.42022259]);
 
 
 
-# Define molar Gibbs energy for (Al,Ti)2(O)3
-@variables YM2O30AL YM2O30TI YM2O31O T
+# # Define molar Gibbs energy for (Al,Ti)2(O)3
+# @variables YM2O30AL YM2O30TI YM2O31O T
 
-G_M_AL2O3 = -1772163.19+1053.4548*T-156.058*T*log(T)+.00709105*T^2-6.29402E-07*T^3 +12366650*T^(-1)
-G_M_TI2O3 = -1581243.06+2395423.68*T^(-1)+940.164783*T-147.673862*T*log(T)-.00173711312*T^2-1.53383348E-10*T^3+19508-2.4214*T
+# G_M_AL2O3 = -1772163.19+1053.4548*T-156.058*T*log(T)+.00709105*T^2-6.29402E-07*T^3 +12366650*T^(-1)
+# G_M_TI2O3 = -1581243.06+2395423.68*T^(-1)+940.164783*T-147.673862*T*log(T)-.00173711312*T^2-1.53383348E-10*T^3+19508-2.4214*T
 
-G_M = G_M_AL2O3*YM2O30AL + G_M_TI2O3*YM2O30TI + R*T*(2*YM2O30AL*log(YM2O30AL) + 2*YM2O30TI*log(YM2O30TI) + 3*YM2O31O*log(YM2O31O))
-G_m = G_M/(2*YM2O30AL + 2*YM2O30TI + 3*YM2O31O)
-# Formula
-mass = [2*YM2O30AL, 3*YM2O31O, 2*YM2O30TI] # moles(Al), moles(O), moles(Ti)
+# G_M = G_M_AL2O3*YM2O30AL + G_M_TI2O3*YM2O30TI + R*T*(2*YM2O30AL*log(YM2O30AL) + 2*YM2O30TI*log(YM2O30TI) + 3*YM2O31O*log(YM2O31O))
+# G_m = G_M/(2*YM2O30AL + 2*YM2O30TI + 3*YM2O31O)
+# # Formula
+# mass = [2*YM2O30AL, 3*YM2O31O, 2*YM2O30TI] # moles(Al), moles(O), moles(Ti)
 
-# substitute(G_M, Dict(YM2O30AL => 0.5, YM2O30TI => 0.5, YM2O31O => 1.0, T => 2000.0))
-# substitute(G_M, Dict(YM2O30AL => 1.0, YM2O30TI => 1e-15, YM2O31O => 1.0, T => 2000.0))
-# substitute(G_m, Dict(YM2O30AL => 0.5, YM2O30TI => 0.5, YM2O31O => 1.0, T => 2000.0))
+# # substitute(G_M, Dict(YM2O30AL => 0.5, YM2O30TI => 0.5, YM2O31O => 1.0, T => 2000.0))
+# # substitute(G_M, Dict(YM2O30AL => 1.0, YM2O30TI => 1e-15, YM2O31O => 1.0, T => 2000.0))
+# # substitute(G_m, Dict(YM2O30AL => 0.5, YM2O30TI => 0.5, YM2O31O => 1.0, T => 2000.0))
 
-state_variables = [T]
-site_fractions = [YM2O30AL, YM2O30TI, YM2O31O]
+# state_variables = [T]
+# site_fractions = [YM2O30AL, YM2O30TI, YM2O31O]
 
-prx = PhaseRecord("CORUNDUM", G_M, mass, state_variables, site_fractions)
-compset = CompSet(prx, [0.5, 0.5, 1.0], 0.2)
+# prx = PhaseRecord("CORUNDUM", G_M, mass, state_variables, site_fractions)
+# compset = CompSet(prx, [0.5, 0.5, 1.0], 0.2)
 
-A = get_equilibrium_matrix([compset])
-b = get_equilibrium_soln([compset])
-x = A \ b
-subs_dict = Dict(Dict(zip(compset.phase_rec.site_fractions, compset.Y))..., T => 2000.0)
-substitute.(x, (subs_dict,))
+# A = get_equilibrium_matrix([compset])
+# b = get_equilibrium_soln([compset])
+# x = A \ b
+# subs_dict = Dict(Dict(zip(compset.phase_rec.site_fractions, compset.Y))..., T => 2000.0)
+# substitute.(x, (subs_dict,))
 
-# Using lapack least squares, need to convert A and b into floats first
+# # Using lapack least squares, need to convert A and b into floats first
 
-AA = Symbolics.value.(substitute.(A, (subs_dict,)))
-bb = Symbolics.value.(substitute.(b, (subs_dict,)))
-LinearAlgebra.LAPACK.gelsd!(AA, bb)
+# AA = Symbolics.value.(substitute.(A, (subs_dict,)))
+# bb = Symbolics.value.(substitute.(b, (subs_dict,)))
+# LinearAlgebra.LAPACK.gelsd!(AA, bb)
 
-############################################################
+# ############################################################
 
-# PHASE LIQUID % 1 1 !
-#  CONSTITUENT LIQUID :AL,TI,O : !
-#  PARAM G (LIQUID,AL;0) 1 -10000; 10000 N !
-#  PARAM G (LIQUID,O;0) 1 -40000; 10000 N !
-#  PARAM G (LIQUID,TI;0) 1 -20000; 10000 N !
+# # PHASE LIQUID % 1 1 !
+# #  CONSTITUENT LIQUID :AL,TI,O : !
+# #  PARAM G (LIQUID,AL;0) 1 -10000; 10000 N !
+# #  PARAM G (LIQUID,O;0) 1 -40000; 10000 N !
+# #  PARAM G (LIQUID,TI;0) 1 -20000; 10000 N !
 
-@variables LIQUID0AL LIQUID0O LIQUID0TI T
+# @variables LIQUID0AL LIQUID0O LIQUID0TI T
 
-G_M_LIQUID = -10000*LIQUID0AL + -40000*LIQUID0O + R*T*(LIQUID0AL*log(LIQUID0AL) + LIQUID0O*log(LIQUID0O))
-mass = [LIQUID0AL, LIQUID0O]
-state_variables = [T]
-site_fractions = [LIQUID0AL, LIQUID0O]
-prx = PhaseRecord("LIQUID", G_M_LIQUID, mass, state_variables, site_fractions)
-compset = CompSet(prx, [0.5, 0.5], 1.0)
+# G_M_LIQUID = -10000*LIQUID0AL + -40000*LIQUID0O + R*T*(LIQUID0AL*log(LIQUID0AL) + LIQUID0O*log(LIQUID0O))
+# mass = [LIQUID0AL, LIQUID0O]
+# state_variables = [T]
+# site_fractions = [LIQUID0AL, LIQUID0O]
+# prx = PhaseRecord("LIQUID", G_M_LIQUID, mass, state_variables, site_fractions)
+# compset = CompSet(prx, [0.5, 0.5], 1.0)
 
-# Solve
-A = get_equilibrium_matrix([compset]);
-b = get_equilibrium_soln([compset]);
-# Symbolic solution
-x = A \ b;
-subs_dict = Dict(Dict(zip(compset.phase_rec.site_fractions, compset.Y))..., T => 300.0)
-substitute.(x, (subs_dict,))
-
-
-############################################################
+# # Solve
+# A = get_equilibrium_matrix([compset]);
+# b = get_equilibrium_soln([compset]);
+# # Symbolic solution
+# x = A \ b;
+# subs_dict = Dict(Dict(zip(compset.phase_rec.site_fractions, compset.Y))..., T => 300.0)
+# substitute.(x, (subs_dict,))
 
 
+# ############################################################
 
 
 
-# Numerical linear least squares soln
-AA = Symbolics.value.(substitute.(A, (subs_dict,)));
-bb = Symbolics.value.(substitute.(b, (subs_dict,)));
 
-LinearAlgebra.LAPACK.gelsd!(AA, bb)
+
+# # Numerical linear least squares soln
+# AA = Symbolics.value.(substitute.(A, (subs_dict,)));
+# bb = Symbolics.value.(substitute.(b, (subs_dict,)));
+
+# LinearAlgebra.LAPACK.gelsd!(AA, bb)
