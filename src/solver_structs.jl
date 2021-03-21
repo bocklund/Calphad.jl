8de13520d961cae4abeb3b9193f68f04951b64aa
bocklund <brandonbocklund@gmail.com>
2021-03-20 23:27:55 -0400

    Rewrite phase_matrix code to generally handle arbitrary length sublattices.

--- src/solver_structs.jl
+++ src/solver_structs.jl
@@ -11,31 +11,44 @@ struct PhaseRecord
     inv_phase_matrix
 end
 
-function phase_matrix(hess, num_state_variables, num_site_fractions)
-    num_internal_constraints = 1  # TODO: hardcoded for now
-    num_internal_dof = num_site_fractions
-    phase_mat = Matrix{Num}(undef, num_internal_dof + num_internal_constraints, num_internal_dof + num_internal_constraints)
-    for i in 1:num_internal_dof
-        for j in 1:num_internal_dof
-            phase_mat[i,j] = hess[num_state_variables+i,num_state_variables+j]
+function phase_matrix(hess, num_potentials, num_site_fractions, sublattice_dof)
+    phase_mat = Matrix{Num}(undef, num_site_fractions + length(sublattice_dof), num_site_fractions + length(sublattice_dof))
+    # Fill the upper-left corner with the Hessian terms
+    for i in 1:num_site_fractions
+        for j in 1:num_site_fractions
+            phase_mat[i,j] = hess[num_potentials+i,num_potentials+j]
         end
     end
-    for i in 1:num_internal_constraints
-        for j in 1:num_internal_constraints
-            phase_mat[num_internal_dof+i,:] .= 1.0
-            phase_mat[:,num_internal_dof+j] .= 1.0
-            phase_mat[num_internal_dof+i, num_internal_dof+i] = 0.0
-        end
+    # Fill the remaining rows (and the symmetric terms in the columns)
+    # using the condition that the sum of site fractions is one
+    subl_offset = 0
+    for s in 1:length(sublattice_dof)
+        # The first columns until the offset are zero
+        println("row $(s+num_site_fractions), 1:$subl_offset = 0, $(subl_offset+1):$(subl_offset+sublattice_dof[s]) = 1, $(subl_offset+sublattice_dof[s]+1):end = 0")
+        phase_mat[num_site_fractions+s,1:subl_offset] .= 0.0
+        # rows in symmetric column
+        phase_mat[1:subl_offset,num_site_fractions+s] .= 0.0
+        # The columns for the current sublattice are all one
+        phase_mat[num_site_fractions+s,subl_offset+1:subl_offset+sublattice_dof[s]] .= 1.0
+        # rows in symmetric column
+        phase_mat[subl_offset+1:subl_offset+sublattice_dof[s],num_site_fractions+s] .= 1.0
+        # Remaining columns are zero
+        phase_mat[num_site_fractions+s,subl_offset+sublattice_dof[s]+1:end] .= 0.0
+        # rows in symmetric columns,
+        # some repeated work here, but it's consistent and logically equal
+        phase_mat[subl_offset+sublattice_dof[s]+1:end,num_site_fractions+s] .= 0.0
+        subl_offset += sublattice_dof[s]
     end
+    println(phase_mat)
     return phase_mat
 end
 
 
-function PhaseRecord(phase_name, obj, mass, state_variables, site_fractions)
-    grad = Symbolics.gradient(obj, [state_variables..., site_fractions...])
-    hess = Symbolics.hessian(obj, [state_variables..., site_fractions...])
-    mass_jac = Symbolics.jacobian(mass, [state_variables..., site_fractions...])
-    pm = phase_matrix(hess, length(state_variables), length(site_fractions))
+function PhaseRecord(phase_name, obj, mass, potentials, site_fractions, sublattice_dof)
+    grad = Symbolics.gradient(obj, [potentials..., site_fractions...])
+    hess = Symbolics.hessian(obj, [potentials..., site_fractions...])
+    mass_jac = Symbolics.jacobian(mass, [potentials..., site_fractions...])
+    pm = phase_matrix(hess, length(potentials), length(site_fractions), sublattice_dof)
 
     return PhaseRecord(phase_name,
                        obj,
@@ -43,7 +56,7 @@ function PhaseRecord(phase_name, obj, mass, state_variables, site_fractions)
                        hess,
                        mass,
                        mass_jac,
-                       state_variables,
+                       potentials,
                        site_fractions,
                        pm,
                        LinearAlgebra.inv(pm),
