20f3b7ab2efeb2d6a7c55d71ffea4163e8d59d58
bocklund <brandonbocklund@gmail.com>
2021-03-22 11:18:50 -0400

    Implementation of get_x_A_row_rhs

--- src/_equilibrium_matrix_conditions.jl
+++ src/_equilibrium_matrix_conditions.jl
@@ -192,6 +192,137 @@ function get_N_A_row_rhs(phase_records, el_idx, N_el_sym,
 end
 
 @doc raw"""
+    get_x_A_row_rhs
+
+See the function `get_N_A_row_rhs`. This is the corresponding mole fraction condition.
+
+# Equation
+
+```math
+% Free chemical potential columns (μ_B)
+\sum_{B_{\mathrm{free}}} \sum_\alpha \sum_i \frac{\aleph^\alpha c_{iB}}{N} \left( \frac{\partial M_A^\alpha}{\partial y_i^\alpha} - x_A \sum_C \frac{\partial M_C^\alpha}{\partial y_i^\alpha} \right)  \mu_B & \\
+% Free potential columns (ΔP)
++ \sum_\mathrm{Pot} \sum_\alpha \sum_i \frac{\aleph^\alpha c_{i\mathrm{Pot}}}{N} \left( \frac{\partial M_A^\alpha}{\partial y_i^\alpha} - x_A \sum_C \frac{\partial M_C^\alpha}{\partial y_i^\alpha} \right)  \Delta \mathrm{Pot} & \\
+% Free phase amount columns (Δℵ)
++ \sum_{\beta_\mathrm{free}} \frac{1}{N} \left( M_A^\beta - x_A \sum_C M_C^\beta \right) \Delta \aleph^\beta &
+% Right-hand side (constant)
+% c_iG
+= - \sum_\alpha \sum_i \frac{\aleph^\alpha c_{iG}}{N} \left( \frac{\partial M_A^\alpha}{\partial y_i^\alpha} - x_A \sum_C \frac{\partial M_C^\alpha}{\partial y_i^\alpha} \right) \\
+% Fixed chemical potentials
+& - \sum_{B_{\mathrm{fixed}}} \sum_\alpha \sum_i \frac{\aleph^\alpha c_{iB}}{N} \left( \frac{\partial M_A^\alpha}{\partial y_i^\alpha} - x_A \sum_C \frac{\partial M_C^\alpha}{\partial y_i^\alpha} \right)  \mu_B \\
+% The term for the condition
+& + \left( x_A - \tilde{x}_A \right) \\
+```
+"""
+function get_x_A_row_rhs(phase_records, el_idx, x_A_prescribed,
+                         fixed_chempot_symbols, free_chempot_idxs,
+                         fixed_pot_symbols, free_pot_idxs,
+                         fixed_phase_symbols, free_phase_idxs,
+                         )
+    num_elements = length(phase_records[1].mass)  # assumes number of elements
+
+    # Constuct N and x_A, which will be used in almost all the expresions below
+    N_A = 0.0  # Intermediate, just used for computing x_A
+    for α in 1:length(phase_records)
+        N_A += _ℵ(phase_records[α]) * phase_records[α].mass[el_idx]
+    end
+    N = 0.0
+    for B in 1:num_elements
+        for α in 1:length(phase_records)
+            N += _ℵ(phase_records[α]) * phase_records[α].mass[B]
+        end
+    end
+    x_A = N_A / N
+
+    # TODO: conditions other than chemical potential
+    # Construct the row in the equilibrium matrix
+    soln_size = (length(free_chempot_idxs) + length(free_pot_idxs) + length(free_phase_idxs))
+    row = Array{Num}(undef, soln_size)
+    # Chemical potential columns
+    for B in 1:length(free_chempot_idxs)
+        total = 0.0
+        for α in 1:length(phase_records)
+            prx = phase_records[α]
+            statevar_offset = length(prx.state_variables)
+            for i in 1:length(prx.site_fractions)
+                factor = _ℵ(prx) * c_iA(prx,i,B) / N
+                inner_total = prx.mass_jac[el_idx,statevar_offset+i]
+                for C in 1:num_elements
+                    inner_total -= x_A * prx.mass_jac[C,statevar_offset+i]
+                end
+                total += factor * inner_total
+            end
+        end
+        row[B] = total
+    end
+    # ΔPotential columns
+    col_offset = length(free_chempot_idxs)
+    for pot in 1:length(free_pot_idxs)
+        total = 0.0
+        for α in 1:length(phase_records)
+            prx = phase_records[α]
+            statevar_offset = length(prx.state_variables)
+            for i in 1:length(prx.state_variables)
+                factor = _ℵ(prx) * c_iPot(prx,i,pot) / N
+                inner_total = prx.mass_jac[el_idx,statevar_offset+i]
+                for C in 1:num_elements
+                    inner_total -= x_A * prx.mass_jac[C,statevar_offset+i]
+                end
+                total += factor * inner_total
+            end
+        end
+        row[col_offset+pot] = total
+    end
+    # Δℵ columns
+    col_offset += length(free_pot_idxs)
+    for β in 1:length(free_phase_idxs)
+        prx = phase_records[β]
+        factor = 1 / N
+        inner_total = prx.mass[el_idx]
+        for C in 1:num_elements
+            inner_total -= x_A * prx.mass[C]
+        end
+        row[col_offset+β] = factor * inner_total
+    end
+
+    # construct the right-hand-side term
+    rhs = 0.0
+    # c_iG terms
+    for α in 1:length(phase_records)
+        prx = phase_records[α]
+        statevar_offset = length(prx.state_variables)
+        for i in 1:length(prx.state_variables)
+            factor = _ℵ(prx) * c_iG(prx, i) / N
+            inner_total = prx.mass_jac[el_idx,statevar_offset+i]
+            for C in 1:num_elements
+                inner_total -= x_A * prx.mass_jac[C,statevar_offset+i]
+            end
+            rhs -= factor * inner_total
+        end
+    end
+    # Fixed chemical potentials
+    for B in 1:length(fixed_chempot_symbols)
+        for α in 1:length(phase_records)
+            prx = phase_records[α]
+            statevar_offset = length(prx.state_variables)
+            for i in 1:length(prx.state_variables)
+                factor = _ℵ(prx) * c_iA(prx, i, B) / N
+                inner_total = prx.mass_jac[el_idx,statevar_offset+i]
+                for C in 1:num_elements
+                    inner_total -= x_A * prx.mass_jac[C,statevar_offset+i]
+                end
+                rhs -= factor * inner_total
+            end
+        end
+    end
+    # Prescribed amount
+    rhs += x_A
+    rhs -= x_A_prescribed
+
+    return (row, rhs)
+end
+
+@doc raw"""
     get_N_row_rhs
 
 See the function `get_N_A_row_rhs`. This is conceptually the same as an N_A
@@ -302,8 +433,11 @@ function cond_row_rhs(cond, elements, phases, phase_records, fixed_free_terms)
         el = str_cond[3:end]
         el_idx = findfirst(x -> x == el, elements)
         row, rhs = get_N_A_row_rhs(phase_records, el_idx, cond, fixed_free_terms...)
+    elseif startswith(str_cond, "X_")
+        el = str_cond[3:end]
+        el_idx = findfirst(x -> x == el, elements)
+        row, rhs = get_x_A_row_rhs(phase_records, el_idx, cond, fixed_free_terms...)
     else
-        # TODO: implement X(A) condition row
         throw("Condition for $str_cond is not yet implemented")
     end
 
