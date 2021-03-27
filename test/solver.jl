64baaa3e2a68dd9cbf07b0382cc4da77cd07d552
Brandon Bocklund <brandonbocklund@gmail.com>
2021-03-26 20:30:39 -0400

    Add a pseudo-binary chemical potential condition "does it work" test

--- test/solver.jl
+++ test/solver.jl
@@ -133,3 +133,41 @@ println(compsets)
 println(free_pot_guess)
 
 end
+
+
+let # pseudo-binary chemical potential conditions
+# The phase amount and Oxygen site fraction should remain the same.
+elements = ["A", "B", "O"];
+
+@variables Y_M2O3_A Y_M2O3_B Y_M2O3_O
+G_M2O3 = R*T*(2*Y_M2O3_A*log(Y_M2O3_A) + 2*Y_M2O3_B*log(Y_M2O3_B) + 3*Y_M2O3_O*log(Y_M2O3_O));
+mass_M2O3 = [2*Y_M2O3_A, 2*Y_M2O3_B, 3*Y_M2O3_O];
+prx_M2O3 = PhaseRecord("M2O3", G_M2O3, mass_M2O3, [P, T], [Y_M2O3_A, Y_M2O3_B, Y_M2O3_O], [2, 1]);
+
+phase_records = [prx_M2O3];
+
+# Conditions
+@variables X_B  N MU_O X_O
+condition_dict = Dict(P => 101325.0, T=>1000.0, MU_O=>0.0, X_B=>0.2, N=>1.0)
+cond_keys = [P, T, MU_O, X_B, N]
+
+# Get the symbolic solution and Δy matrix
+sym_soln = Calphad.get_solution(phase_records, elements, cond_keys);
+sym_Delta_y_mats = Calphad.get_Delta_y_mat.(phase_records, (elements,), (cond_keys,));
+
+# Composition sets
+compset_M2O3 = Calphad.CompSet(prx_M2O3, [0.51, 0.49, 1.0], 0.2)
+compsets = [compset_M2O3]
+
+# Solve with printing
+println(condition_dict)
+println(compsets)
+Calphad.solve_and_update(compsets, condition_dict, sym_soln, sym_Delta_y_mats, [1], Dict(); step_size=0.01, doprint=true)
+println(compsets)
+Calphad.solve_and_update(compsets, condition_dict, sym_soln, sym_Delta_y_mats, [1], Dict(); step_size=0.01, doprint=true)
+println(compsets)
+@test !any(isnan.(compsets[1].Y))
+@test compsets[1].ℵ ≈ 0.2
+@test compsets[1].Y[3] ≈ 1.0
+end
+
